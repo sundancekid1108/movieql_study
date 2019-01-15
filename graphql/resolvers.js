02162039d06772d2292ad020ce4efa872419e8b2
Mr. Sundance <sundancekid1108@gmail.com>
2019-01-15 14:17:04 +0900

    GraphQL Server 오픈
    GraphQL Server 오픈

---
+++ graphql/resolvers.js
@@ -0,0 +1,8 @@
+/*Query를 처리하는것 (Resolve) */
+const resolvers = {
+    Query: {
+        name: () => "Its Query name"
+    }
+}
+
+export default resolvers
--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-import { graphQlServer } from "graphql-yoga"
+import { GraphQLServer } from "graphql-yoga"
+import resolvers from "./graphql/resolvers"
+
 console.log("hello")
+
+const server = new GraphQLServer({
+    typeDefs: "graphql/schema.graphql",
+    resolvers: resolvers
+
+})
+
+server.start(() => console.log("graphql server running"))
